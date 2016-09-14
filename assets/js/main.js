11d9ab8ad7d97c697df4554c12d52e7dda4d6c24
Neta Elad <bananushka@gmail.com>
2016-09-14 16:14:01 +0300

    Adding files

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function () {
+  var NS = {};
+  window.NS = NS;
+
+  NS.toggle = function toggle(element) {
+    if (element.paused) {
+      element.play();
+    } else {
+      element.pause();
+    }
+  };
+
+
+  (function addBlankToLinks() {
+    var links = document.links;
+
+    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+      if (links[i].hostname != window.location.hostname) {
+         links[i].target = '_blank';
+       }
+    }
+  }());
+}());
