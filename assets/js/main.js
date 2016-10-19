351c2c68e365a356c2c9a0a0aeae189b3e464b49
Neta Elad <bananushka@gmail.com>
2016-10-19 23:02:35 +0300

    First project draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@
     for (var i = 0, linksLength = links.length; i < linksLength; i++) {
       if (links[i].hostname != window.location.hostname) {
          links[i].target = '_blank';
-       }
+      }
     }
   }());
 }());
